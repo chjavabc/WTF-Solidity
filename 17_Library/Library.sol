c5d18c9b3104365141d70bfadc811f0f638e861e
chenhao0339 <395944645@163.com>
2024-08-10 12:24:08 +0800

    library

--- 17_Library/Library.sol
+++ 17_Library/Library.sol
@@ -62,6 +62,7 @@ library Strings {
 }
 
 
+
 // 用函数调用另一个库合约
 contract UseLibrary{    
     // 利用using for操作使用库
@@ -74,5 +75,35 @@ contract UseLibrary{
     // 直接通过库合约名调用
     function getString2(uint256 _number) public pure returns(string memory){
         return Strings.toHexString(_number);
+    }
+}
+
+
+
+// 用函数调用另一个库合约
+contract UseLibrary{    
+    // 利用using for操作使用库
+    using Strings for uint256;
+    function getString1(uint256 _number) public pure returns(string memory){
+        // 库函数会自动添加为uint256型变量的成员
+        return _number.toHexString();
+    }
+
+    // 直接通过库合约名调用
+    function getString2(uint256 _number) public pure returns(string memory){
+        return Strings.toHexString(_number);
+    }
+}
+
+
+library StorageSlot {
+    struct StringSlot {
+    string value;
+    }
+    function getStringSlot(bytes32 slot) internal pure returns (StringSlot
+    storage r) {
+    assembly {
+    r.slot := slot
+    }
     }
 }
